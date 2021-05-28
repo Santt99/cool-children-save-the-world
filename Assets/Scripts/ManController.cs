91871ee9cc6075927a6195570bdbc82ed14bb5f5
cubomx <cheservelram657@gmail.com>
2021-05-28 02:04:30 -0500

    UPDATE: I tried to implement another voice recognizer. TODO: I couldn't test it, so please try it on your phone

--- Assets/Scripts/ManController.cs
+++ Assets/Scripts/ManController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TextSpeech;
 
 
 public class ManController : MonoBehaviour
@@ -25,41 +24,11 @@ public class ManController : MonoBehaviour
     private GameController gameController = new GameController();
     void Start()
     {
-        Setting("en-US");
-        SpeechToText.instance.onResultCallback = OnResultSpeech;
-        StartRecording();
+        
     }
 
-    public void Setting(string code)
-    {
-        TextToSpeech.instance.Setting(code, pitch, rate);
-        SpeechToText.instance.Setting(code);
-    }
-    public void StartRecording()
-    {
-#if UNITY_EDITOR
-#else
-        SpeechToText.instance.StartRecording("Speak any");
-#endif
-    }
 
-    public void StopRecording()
-    {
-#if UNITY_EDITOR
-        OnResultSpeech("Not support in editor.");
-#else
-        SpeechToText.instance.StopRecording();
-#endif
-
-    }
-    void OnResultSpeech(string _data)
-    {
-        if (_data == "No")
-        {
-            keepMovingUp = false;
-        }
-
-    }
+    
 
     // Update is called once per frame
     void Update()
@@ -93,7 +62,6 @@ public class ManController : MonoBehaviour
         }
         if (!keepAlive)
         {
-            StopRecording();
             Destroy(this.gameObject);
         }
 
